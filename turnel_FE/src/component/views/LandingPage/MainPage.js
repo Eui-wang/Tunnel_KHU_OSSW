577958c97496d04e367deb6e1a36e1dfaea3c41f
wang323@khu.ac.kr <wang323@khu.ac.kr>
2021-11-30 02:25:39 +0900

    MainPage ver1.0

--- turnel_FE/src/component/views/LandingPage/MainPage.js
+++ turnel_FE/src/component/views/LandingPage/MainPage.js
@@ -1,4 +1,4 @@
-import { Button, Input } from "semantic-ui-react"
+import {Button, Grid, Image, Segment} from "semantic-ui-react"
 import "../style/MainPage.scss";
 function MainPage() {
     return (
@@ -8,17 +8,60 @@ function MainPage() {
                     <h1>Tunnel</h1>
                 </div>
                 <div className="None-title">
-                    <Button class="ui button">
+                    <Button className="ui right floated button">
                         Logout
                     </Button>
                 </div>
             </div>
             <div className="Main-body">
-                <div className="contents">
-                    <h1>a</h1>
+                <div className="contents-container">
+                    <div className="write-button">
+                        <Button className="ui animated button"
+                                tabIndex="0">
+                            <div className="visible content">새 고민 작성하기</div>
+                            <div className="hidden content">
+                                <i className="pencil alternate icon"></i>
+                            </div>
+                        </Button>
+                    </div>
+                    <div className="contents">
+                        <div className="ui segment">
+                            <p>a</p>
+                        </div>
+                        <div className="ui segment">
+                            <p>a</p>
+                        </div>
+                        <div className="ui segment">
+                            <p>a</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="user">
-                    <h1>a</h1>
+                <div className="user-container">
+                    <div className="userInfo">
+                        <h1>User ID</h1>
+                    </div>
+                    <div className="checkIssue-button">
+                        <Button size="Big" className="ui animated button"
+                                tabIndex="0">
+                            <div className="visible content">도착한 글</div>
+                            <div className="hidden content">
+                                <i className="pencil alternate icon"></i>
+                            </div>
+                        </Button>
+                    </div>
+                    <div className="user">
+                        <div className="Answer">
+                            <div className="ui segment">
+                                <p>a</p>
+                            </div>
+                            <div className="ui segment">
+                                <p>a</p>
+                            </div>
+                            <div className="ui segment">
+                                <p>a</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

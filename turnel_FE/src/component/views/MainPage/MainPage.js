9762ca35a9444b83bf4c9b4e7443326e83f75656
wang323@khu.ac.kr <wang323@khu.ac.kr>
2021-12-05 00:07:45 +0900

    Board ver1.0

--- turnel_FE/src/component/views/MainPage/MainPage.js
+++ turnel_FE/src/component/views/MainPage/MainPage.js
@@ -1,8 +1,8 @@
 import {Button} from "semantic-ui-react"
 import "../style/MainPage.scss";
 import { useNavigate } from "react-router-dom";
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import Board from "../Board/Board"
+import React from "react";
 
 function MainPage() {
     const navigate = useNavigate();
@@ -22,55 +22,17 @@ function MainPage() {
                 </div>
             </div>
             <div className="Main-body">
-                <div className="contents-container">
-                    <div className="write-button">
-                        <Button className="ui animated button"
-                                tabIndex="0">
-                            <div className="visible content">새 고민 작성하기</div>
-                            <div className="hidden content">
-                                <i className="pencil alternate icon"></i>
-                            </div>
-                        </Button>
-                    </div>
-                    <div className="contents">
-                        <div className="ui segment">
-                            <CKEditor
-                                editor={ClassicEditor}
-                                data="<p>Hello from CKEditor 5!</p>"
-                                onReady={editor => {
-                                    // You can store the "editor" and use when it is needed.
-                                    console.log('Editor is ready to use!', editor);
-                                }}
-                                onChange={(event, editor) => {
-                                    const data = editor.getData();
-                                    console.log({ event, editor, data });
-                                }}
-                                onBlur={(event, editor) => {
-                                    console.log('Blur.', editor);
-                                }}
-                                onFocus={(event, editor) => {
-                                    console.log('Focus.', editor);
-                                }}
-                            />
-                        </div>
-                        <div className="ui segment">
-                            <p>a</p>
-                        </div>
-                        <div className="ui segment">
-                            <p>a</p>
-                        </div>
-                    </div>
-                </div>
+                <Board/>
                 <div className="user-container">
                     <div className="userInfo">
                         <h1>User ID</h1>
                     </div>
                     <div className="checkIssue-button">
-                        <Button size="Big" className="ui animated button"
+                        <Button className="ui animated button"
                                 tabIndex="0">
                             <div className="visible content">도착한 글</div>
                             <div className="hidden content">
-                                <i className="paper plane ic    on"></i>
+                                <i className="paper plane icon"></i>
                             </div>
                         </Button>
                     </div>

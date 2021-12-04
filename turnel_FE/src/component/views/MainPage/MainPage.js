6b8e5f3f3e311faa103e5e013dcfb4abf68f150f
wang323@khu.ac.kr <wang323@khu.ac.kr>
2021-12-04 21:41:33 +0900

    Set reducer & Modify user-action

--- turnel_FE/src/component/views/MainPage/MainPage.js
+++ turnel_FE/src/component/views/MainPage/MainPage.js
@@ -1,6 +1,8 @@
 import {Button} from "semantic-ui-react"
 import "../style/MainPage.scss";
 import { useNavigate } from "react-router-dom";
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 function MainPage() {
     const navigate = useNavigate();
@@ -32,7 +34,24 @@ function MainPage() {
                     </div>
                     <div className="contents">
                         <div className="ui segment">
-                            <p>a</p>
+                            <CKEditor
+                                editor={ClassicEditor}
+                                data="<p>Hello from CKEditor 5!</p>"
+                                onReady={editor => {
+                                    // You can store the "editor" and use when it is needed.
+                                    console.log('Editor is ready to use!', editor);
+                                }}
+                                onChange={(event, editor) => {
+                                    const data = editor.getData();
+                                    console.log({ event, editor, data });
+                                }}
+                                onBlur={(event, editor) => {
+                                    console.log('Blur.', editor);
+                                }}
+                                onFocus={(event, editor) => {
+                                    console.log('Focus.', editor);
+                                }}
+                            />
                         </div>
                         <div className="ui segment">
                             <p>a</p>
@@ -51,7 +70,7 @@ function MainPage() {
                                 tabIndex="0">
                             <div className="visible content">도착한 글</div>
                             <div className="hidden content">
-                                <i className="pencil alternate icon"></i>
+                                <i className="paper plane ic    on"></i>
                             </div>
                         </Button>
                     </div>
